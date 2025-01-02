d5d902e89170fc0e6be993d10f6f6b91f5eda8e3
MargaCM06 <marianocaliya06@gmail.com>
2025-01-02 22:50:34 +0800

    Update settings.py

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -77,12 +77,14 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '<margacm06'>$<databasename>',
-        'USER': '<MargaCM06'>,
-        'PASSWORD': 'Caliya06.',
-        'HOST': 'margacm06.mysql.pythonanywhere-services.com',
+        'NAME': 'elderco',
+        'USER' : 'root',
+        'PASSWORD' : 'password',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -124,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
